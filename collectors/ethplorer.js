7df9da9a2948051b5f52680b52f44a475a17f3dc
Robbie Pallas <robbie.pallas@piksel.com>
2018-01-31 23:50:20 +0000

    organise metrics

--- collectors/ethplorer.js
+++ collectors/ethplorer.js
@@ -6,14 +6,16 @@ function gauge(name, help, labels=['address']) {
   return new Gauge({ name: `ethplorer_account_${name}`, help: help, labelNames: labels })
 }
 
-const ethBalanceGauge = gauge('eth_balance', 'Total balance of either in account');
-const ethTotalInGauge = gauge('eth_totalIn', 'Total either transferred into account');
-const ethTotalOutGauge = gauge('eth_totalOut', 'Total either transferred out of account');
-const txnTotalGauge = gauge('txn_total', 'Total number of transactons for account');
-const tokenHolderCountGauge = gauge('token_balance', 'Balance of specific token for account', ['address', 'symbol']);
-const tokenBalanceGauge = gauge('token_balance', 'Balance of specific token for account', ['address', 'symbol']);
-const tokenBalanceUSDGauge = gauge('token_balance_usd', 'Balance of specific token for account in USD', ['address', 'symbol']);
-const tokenBalanceTotalGauge = gauge('token_balance_total_usd', 'Overall token balance for account in USD');
+const metrics = {
+  ethBalance: gauge('eth_balance', 'Total balance of either in account');
+  ethTotalIn: gauge('eth_totalIn', 'Total either transferred into account');
+  ethTotalOut: gauge('eth_totalOut', 'Total either transferred out of account');
+  txnTotal: gauge('txn_total', 'Total number of transactons for account');
+  tokenHolderCount: gauge('token_balance', 'Balance of specific token for account', ['address', 'symbol']);
+  tokenBalance: gauge('token_balance', 'Balance of specific token for account', ['address', 'symbol']);
+  tokenBalanceUSD: gauge('token_balance_usd', 'Balance of specific token for account in USD', ['address', 'symbol']);
+  tokenBalanceTotal: gauge('token_balance_total_usd', 'Overall token balance for account in USD');
+};
 
 async function scrape() {
   const address = process.env.CRYPTO_EXPORTER_ADDRESS || '';
@@ -31,21 +33,21 @@ async function scrape() {
 function arrange(response) {
   const { address, ETH, countTxs, tokens } = response.body;
 
-  ethBalanceGauge.set({ address: address }, ETH.balance);
-  ethTotalInGauge.set({ address: address }, ETH.totalIn);
-  ethTotalOutGauge.set({ address: address }, ETH.totalOut);
-  txnTotalGauge.set({ address: address }, countTxs);
+  metrics.ethBalance.set({ address: address }, ETH.balance);
+  metrics.ethTotalIn.set({ address: address }, ETH.totalIn);
+  metrics.ethTotalOut.set({ address: address }, ETH.totalOut);
+  metrics.txnTotal.set({ address: address }, countTxs);
 
   let overallTokenBalanceUSD = 0;
   tokens.forEach(token => {
     const symbol = token.tokenInfo.symbol;
     const tokenBalanceUSD = token.tokenInfo.price ? (Number(token.balance) * token.tokenInfo.price.rate) / Number(`1e${token.tokenInfo.decimals}`) : 0;
     overallTokenBalanceUSD += tokenBalanceUSD;
-    tokenBalanceGauge.set({ address: address, symbol: symbol }, Number(token.balance) / Number(`1e${token.tokenInfo.decimals}`));
-    tokenBalanceUSDGauge.set({ address: address, symbol: symbol }, Number(tokenBalanceUSD));
-    tokenHolderCountGauge.set({ address: address, symbol: symbol }, Number(token.tokenInfo.holdersCount));
+    metrics.tokenBalance.set({ address: address, symbol: symbol }, Number(token.balance) / Number(`1e${token.tokenInfo.decimals}`));
+    metrics.tokenBalanceUSD.set({ address: address, symbol: symbol }, Number(tokenBalanceUSD));
+    metrics.tokenHolderCount.set({ address: address, symbol: symbol }, Number(token.tokenInfo.holdersCount));
   });
-  tokenBalanceTotalGauge.set({ address: address }, overallTokenBalanceUSD);
+  metrics.tokenBalanceTotal.set({ address: address }, overallTokenBalanceUSD);
 
   console.log(`${new Date().toISOString()} - ethplorer metrics collected`);
 }
